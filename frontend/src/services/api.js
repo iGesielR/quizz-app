83aea81d08dec960c341d33119bb7502698a36b1
iCRISS <cristiangon020600@gmail.com>
2026-06-22 20:57:33 -0600

    Ajuste de Axios para leer variables de entorno en producción

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:5000/api"
+  // Intenta leer la variable de internet, si no existe (como en tu PC local), usa localhost
+  baseURL: import.meta.env.VITE_API_URL || "https://quiz-app-backend-enj1.onrender.com/api"
 });
